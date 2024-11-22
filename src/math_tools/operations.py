a5932e691664acd402ba6837141c7069923b9996
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-11-21 22:36:39 -0500

    added base add functionality

--- src/math_tools/operations.py
+++ src/math_tools/operations.py
@@ -1,12 +1,15 @@
 import math
 
-def add(a, b) -> float:
+#TO-DO: Improve the add function by making it so that given any amount of floats, you can find the total of it
+def add(a: float, b: float) -> float:
     """
 
     Args:
-        a:
-        b:
+        a: (float) The first number
+        b: (float) The second number
 
     Returns:
-
+        sum: (float) The sum of the two numbers
     """
+
+    return a + b
